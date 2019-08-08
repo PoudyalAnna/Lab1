c6f5a11ccaf2f5b2a155168d6541e944243a849c
anita poudyal <poudyalanita@yahoo.com>
2019-08-08 08:53:04 -0500

    Change some code

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -10,9 +10,41 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
-            Console.WriteLine("Hello From Anita");
-            Console.WriteLine(" lets change againg");
+            // variable declaration
+            string strPlayerName;
+            double nhits, nAtBat;
+            double dblBayyingAverage;
+
+            Console.WriteLine("This program calculate a baseball playet average..");
+            Console.WriteLine("----------------------->");
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Enter the Players name:   ");
+            strPlayerName = Console.ReadLine();
+
+            // prompt for hits, walk and at bats
+            Console.WriteLine("Enter the Players hits:   ");
+            nhits = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Players  # of at bats:   ");
+            nAtBat = Convert.ToInt32(Console.ReadLine());
+
+
+            // calculating 
+            dblBayyingAverage = nhits % nAtBat;
+            Console.WriteLine(strPlayerName + "'S batting average is " + dblBayyingAverage);
+            Console.ReadLine();
+
+
+            Console.WriteLine("My Dog Gadget is " + 5 + 5 + " years old");
+           
+            Console.ReadLine();
+
+            Console.WriteLine("My Dog Gadget is {0} years old.", 5 + 5);
+            Console.WriteLine("I paid {0:c} for her.", 20);
+
         }
-    }
+
+    } 
 }
+
